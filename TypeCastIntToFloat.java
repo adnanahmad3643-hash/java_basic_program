8cfa30d69dc0bd9e4c1bcd910983e88e6c9cb787
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:26:08 +0530

    Add files via upload

---
+++ TypeCastIntToFloat.java
@@ -0,0 +1,10 @@
+public class TypeCastIntToFloat {
+    public static void main(String[] args) {
+        int num=1000;
+        float f=(float) num;// TYPE CAST INT TO FLOAT 
+        float value=399.90f;
+        int temp=(int) value;//TYPE CAST FLOAT TO INT.
+        
+
+    }
+}
