05863b31926c5d06b23fb204015c6ecfbce7ad95
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:20:33 +0530

    Add files via upload

---
+++ Perimiter.java
@@ -0,0 +1,8 @@
+public class Perimiter {
+    public static void main(String[] args) {
+        int length=2;
+        int width=2;
+        int perimiter=2*(length+width);
+        System.out.println("PERIMITER OF RECT ANGLE "+perimiter);
+    }
+}
