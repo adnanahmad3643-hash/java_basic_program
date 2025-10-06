1a11a6832a81e42391dc27c61ec665e45cd002b8
Adnan Ahmad <adnanahmad3643@gmail.com>
2025-10-06 18:20:11 +0530

    Add files via upload

---
+++ Swaping.java
@@ -0,0 +1,15 @@
+public class Swaping {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a+" \n "+b);
+        // ..........OR.........
+        // int temp=a;
+        // a=b;
+        // b=temp;
+
+    }
+}
